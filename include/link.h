59af8c3fdd1d8a4b248601efe07d427637325fe3
Mateusz Urbańczyk <mu@qed.ai>
2021-11-01 11:20:16 +0100

    Links

---
+++ include/link.h
@@ -0,0 +1,10 @@
+typedef struct Link {
+    struct Link* prev;
+    struct Link* next;
+} link_t;
+
+void link_init(link_t* link);
+
+void link_insert(link_t* link, link_t* base);
+
+void link_remove(link_t* link);
