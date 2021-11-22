f599abe56b245c60a101539056d51064147c5ff3
Mateusz Urbańczyk <mu@qed.ai>
2021-11-22 20:18:45 +0100

    Lint tm.hpp header

--- include/tm.hpp
+++ include/tm.hpp
@@ -21,7 +21,7 @@
  *
  * Interface declaration for the transaction manager to use (C++ version).
  * YOU SHOULD NOT MODIFY THIS FILE.
-**/
+ **/
 
 #pragma once
 
@@ -30,8 +30,9 @@
 
 // -------------------------------------------------------------------------- //
 
-using shared_t = void*; // The type of a shared memory region
-constexpr static shared_t invalid_shared = nullptr; // Invalid shared memory region
+using shared_t = void *; // The type of a shared memory region
+constexpr static shared_t invalid_shared =
+    nullptr; // Invalid shared memory region
 
 // Note: a uintptr_t is an unsigned integer that is big enough to store an
 // address. Said differently, you can either use an integer to identify
@@ -40,24 +41,24 @@ constexpr static shared_t invalid_shared = nullptr; // Invalid shared memory reg
 using tx_t = uintptr_t; // The type of a transaction identifier
 constexpr static tx_t invalid_tx = ~(tx_t(0)); // Invalid transaction constant
 
-enum class Alloc: int {
-    success = 0, // Allocation successful and the TX can continue
-    abort   = 1, // TX was aborted and could be retried
-    nomem   = 2  // Memory allocation failed but TX was not aborted
+enum class Alloc : int {
+  success = 0, // Allocation successful and the TX can continue
+  abort = 1,   // TX was aborted and could be retried
+  nomem = 2    // Memory allocation failed but TX was not aborted
 };
 
 // -------------------------------------------------------------------------- //
 
 extern "C" {
-    shared_t tm_create(size_t, size_t) noexcept;
-    void     tm_destroy(shared_t) noexcept;
-    void*    tm_start(shared_t) noexcept;
-    size_t   tm_size(shared_t) noexcept;
-    size_t   tm_align(shared_t) noexcept;
-    tx_t     tm_begin(shared_t, bool) noexcept;
-    bool     tm_end(shared_t, tx_t) noexcept;
-    bool     tm_read(shared_t, tx_t, void const*, size_t, void*) noexcept;
-    bool     tm_write(shared_t, tx_t, void const*, size_t, void*) noexcept;
-    Alloc    tm_alloc(shared_t, tx_t, size_t, void**) noexcept;
-    bool     tm_free(shared_t, tx_t, void*) noexcept;
+shared_t tm_create(size_t, size_t) noexcept;
+void tm_destroy(shared_t) noexcept;
+void *tm_start(shared_t) noexcept;
+size_t tm_size(shared_t) noexcept;
+size_t tm_align(shared_t) noexcept;
+tx_t tm_begin(shared_t, bool) noexcept;
+bool tm_end(shared_t, tx_t) noexcept;
+bool tm_read(shared_t, tx_t, void const *, size_t, void *) noexcept;
+bool tm_write(shared_t, tx_t, void const *, size_t, void *) noexcept;
+Alloc tm_alloc(shared_t, tx_t, size_t, void **) noexcept;
+bool tm_free(shared_t, tx_t, void *) noexcept;
 }
